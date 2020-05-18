4508d9a4b785374525b717b8c5e14b1bb644ee62
dgrisham <dmgrisham@gmail.com>
2020-05-18 17:46:01 -0600

    log -> printf

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"math/rand"
 	"net/http"
 	"os"
@@ -39,12 +38,14 @@ func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 
 	var authCode string
 	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code %v", err)
+		fmt.Printf("Unable to read authorization code %v", err)
+		os.Exit(1)
 	}
 
 	tok, err := config.Exchange(context.TODO(), authCode)
 	if err != nil {
-		log.Fatalf("Unable to retrieve token from web %v", err)
+		fmt.Printf("Unable to retrieve token from web %v", err)
+		os.Exit(1)
 	}
 	return tok
 }
@@ -66,7 +67,8 @@ func saveToken(path string, token *oauth2.Token) {
 	fmt.Printf("Saving credential file to: %s\n", path)
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
+		fmt.Printf("Unable to cache oauth token: %v", err)
+		os.Exit(1)
 	}
 	defer f.Close()
 	json.NewEncoder(f).Encode(token)
@@ -75,19 +77,22 @@ func saveToken(path string, token *oauth2.Token) {
 func main() {
 	b, err := ioutil.ReadFile("credentials.json")
 	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
+		fmt.Printf("Unable to read client secret file: %v", err)
+		os.Exit(1)
 	}
 
 	// If modifying these scopes, delete your previously saved token.json.
 	config, err := google.ConfigFromJSON(b, drive.DriveReadonlyScope)
 	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
+		fmt.Printf("Unable to parse client secret file to config: %v", err)
+		os.Exit(1)
 	}
 	client := getClient(config)
 
 	srv, err := drive.New(client)
 	if err != nil {
-		log.Fatalf("Unable to retrieve Drive client: %v", err)
+		fmt.Printf("Unable to retrieve Drive client: %v", err)
+		os.Exit(1)
 	}
 
 	r, err := srv.Files.List().PageSize(10).Fields("nextPageToken, files(id, name)").Do()
