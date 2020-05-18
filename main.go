782a63b530905aa0275bc2901cea3e1fce3a20bc
dgrisham <dmgrisham@gmail.com>
2020-05-18 17:19:54 -0600

    change permission

--- main.go
+++ main.go
@@ -79,7 +79,7 @@ func main() {
 	}
 
 	// If modifying these scopes, delete your previously saved token.json.
-	config, err := google.ConfigFromJSON(b, drive.DriveMetadataReadonlyScope)
+	config, err := google.ConfigFromJSON(b, drive.DriveReadonlyScope)
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
