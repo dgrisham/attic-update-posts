5657e386f45c839a69ba6351d642071d0813d15c
dgrisham <dmgrisham@gmail.com>
2020-05-18 18:06:17 -0600

    use logrus

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.14
 
 require (
 	github.com/gorilla/mux v1.7.4
+	github.com/sirupsen/logrus v1.6.0
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/api v0.24.0
 )
--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
@@ -33,19 +34,17 @@ func getClient(config *oauth2.Config) *http.Client {
 // Request a token from the web, then returns the retrieved token.
 func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
+	logrus.Infof("Go to the following link in your browser then type the "+
 		"authorization code: \n%v\n", authURL)
 
 	var authCode string
 	if _, err := fmt.Scan(&authCode); err != nil {
-		fmt.Printf("Unable to read authorization code %v", err)
-		os.Exit(1)
+		logrus.Fatalf("Unable to read authorization code %v", err)
 	}
 
 	tok, err := config.Exchange(context.TODO(), authCode)
 	if err != nil {
-		fmt.Printf("Unable to retrieve token from web %v", err)
-		os.Exit(1)
+		logrus.Fatalf("Unable to retrieve token from web %v", err)
 	}
 	return tok
 }
@@ -64,11 +63,10 @@ func tokenFromFile(file string) (*oauth2.Token, error) {
 
 // Saves a token to a file path.
 func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
+	logrus.Infof("Saving credential file to: %s\n", path)
 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
-		fmt.Printf("Unable to cache oauth token: %v", err)
-		os.Exit(1)
+		logrus.Fatalf("Unable to cache oauth token: %v", err)
 	}
 	defer f.Close()
 	json.NewEncoder(f).Encode(token)
@@ -77,32 +75,27 @@ func saveToken(path string, token *oauth2.Token) {
 func main() {
 	b, err := ioutil.ReadFile("credentials.json")
 	if err != nil {
-		fmt.Printf("Unable to read client secret file: %v", err)
-		os.Exit(1)
+		logrus.Fatalf("Unable to read client secret file: %v", err)
 	}
 
 	// If modifying these scopes, delete your previously saved token.json.
 	config, err := google.ConfigFromJSON(b, drive.DriveReadonlyScope)
 	if err != nil {
-		fmt.Printf("Unable to parse client secret file to config: %v", err)
-		os.Exit(1)
+		logrus.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
 	client := getClient(config)
 
 	srv, err := drive.New(client)
 	if err != nil {
-		fmt.Printf("Unable to retrieve Drive client: %v", err)
-		os.Exit(1)
+		logrus.Fatalf("Unable to retrieve Drive client: %v", err)
 	}
 
 	r, err := srv.Files.List().PageSize(10).Fields("nextPageToken, files(id, name)").Do()
 	if err != nil {
-		fmt.Printf("Unable to retrieve files: %v", err)
-		os.Exit(1)
+		logrus.Fatalf("Unable to retrieve files: %v", err)
 	}
 	if len(r.Files) == 0 {
-		fmt.Println("No files found.")
-		os.Exit(1)
+		logrus.Fatalf("No files found.")
 	} else {
 		for _, file := range r.Files {
 			if file.Name == "attic-posts" {
@@ -116,10 +109,10 @@ func main() {
 				}
 				channel, err := srv.Files.Watch(file.Id, channel).Do()
 				if err != nil {
-					fmt.Printf("error: %v\n", err)
+					logrus.Infof("error: %v\n", err)
 				}
-				fmt.Printf("channel.ServerResponse: %v\n", channel.ServerResponse)
-				fmt.Printf("channel.ServerResponse.Header: %v\n", channel.ServerResponse.Header)
+				logrus.Infof("channel.ServerResponse: %v\n", channel.ServerResponse)
+				logrus.Infof("channel.ServerResponse.Header: %v\n", channel.ServerResponse.Header)
 				startHTTPListener()
 			}
 		}
@@ -130,12 +123,11 @@ func startHTTPListener() {
 	router := mux.NewRouter()
 	fmt.Println("starting http listener...")
 	router.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Printf("req: %v\n", r)
+		logrus.Infof("req: %v\n", r)
 		w.WriteHeader(http.StatusOK)
 	})
 	if err := http.ListenAndServe(":9000", router); err != nil {
-		fmt.Printf("error starting http listener: %v\n", err)
-		os.Exit(1)
+		logrus.Fatalf("error starting http listener: %v\n", err)
 	}
 }
 
