b96e294de35e61dc5fd410d9fb358551aa9ff484
dgrisham <dmgrisham@gmail.com>
2020-05-23 12:19:56 -0600

    Download update post file

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"math/rand"
 	"net/http"
 	"os"
 	"strings"
@@ -13,7 +10,6 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
-	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
 )
@@ -24,8 +20,6 @@ type Post struct {
 	Filename string
 	Channel  *drive.Channel
 }
-
-var srv *drive.Service
 
 func main() {
 	logrus.SetFormatter(&logrus.JSONFormatter{PrettyPrint: true})
@@ -47,10 +41,16 @@ func main() {
 		logrus.WithError(err).Fatal("Unable to retrieve Drive client")
 	}
 
+	posts := subscribeToPosts()
+	startHTTPListener(posts)
+}
+
+func subscribeToPosts() map[string]Post {
 	r, err := srv.Files.List().PageSize(10).Fields("nextPageToken, files(id, name)").Do()
 	if err != nil {
 		logrus.WithError(err).Fatal("Unable to retrieve file list")
 	}
+
 	if len(r.Files) == 0 {
 		logrus.Fatal("No files found.")
 	} else {
@@ -64,6 +64,10 @@ func main() {
 					logrus.WithError(err).Fatal("Error listing author folders")
 				}
 
+				/*************************
+				* get all author folders *
+				*************************/
+
 				for _, author := range authorFolders.Files {
 					logrus.WithField("author", author.Name).Debug("Retrieving posts for author")
 					dateFolders, err := srv.Files.List().
@@ -72,6 +76,10 @@ func main() {
 					if err != nil {
 						logrus.WithError(err).WithField("author", author).Fatal("Error listing author post folders")
 					}
+
+					/**********************************
+					* get all post folders for author *
+					**********************************/
 
 					for _, date := range dateFolders.Files {
 						logrus.WithField("date", date.Name).Debug("Retrieving posts for author")
@@ -88,6 +96,10 @@ func main() {
 								"expected": 1,
 							}).Error("Unexpected number of post files")
 						}
+
+						/************************************
+						* subscribe to updates on post file *
+						************************************/
 
 						postFile := postFiles.Files[0]
 
@@ -123,16 +135,29 @@ func main() {
 						posts[returnedChannel.Id] = post
 					}
 				}
-				startHTTPListener(posts)
+
+				return posts
 			}
 		}
 	}
+
+	return nil
 }
 
 func startHTTPListener(posts map[string]Post) {
 	router := mux.NewRouter()
-	fmt.Println("starting http listener...")
-	router.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
+	logrus.Info("Starting http listener...")
+
+	router.HandleFunc("/api", HandlePostUpdate(posts))
+	router.HandleFunc("/api/stop", HandleStop(posts))
+
+	if err := http.ListenAndServe(":9000", router); err != nil {
+		logrus.WithError(err).Fatal("error starting http listener")
+	}
+}
+
+func HandlePostUpdate(posts map[string]Post) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			logrus.WithError(err).Error("Error reading request body")
@@ -159,8 +184,6 @@ func startHTTPListener(posts map[string]Post) {
 			return
 		}
 
-		logrus.Info("Handling update request")
-
 		id := r.Header.Get("X-Goog-Channel-ID")
 		post, ok := posts[id]
 		if !ok {
@@ -172,13 +195,46 @@ func startHTTPListener(posts map[string]Post) {
 			"state":   state,
 			"changes": changes,
 			"post":    post,
-		}).Info("Received update notification for post")
+		}).Debug("Received update notification for post")
+
+		downloadDriveFile(post)
 
 		return
-	})
+	}
+}
 
-	router.HandleFunc("/api/stop", func(w http.ResponseWriter, r *http.Request) {
+func downloadDriveFile(post Post) {
+	log := logrus.WithField("post", post)
+	log.Info("Handling update request")
+
+	req, err := http.NewRequest("GET", post.Channel.ResourceUri, nil)
+	if err != nil {
+		log.WithError(err).Error("Failed to create GET request for updated post file")
+		return
+	}
+
+	resp, err := http.DefaultTransport.RoundTrip(req)
+	defer resp.Body.Close()
+	if err != nil {
+		log.WithError(err).Error("Failed to fetch updated post file")
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.WithError(err).Error("Failed to read response body")
+		return
+	}
+
+	log.WithField("body", body).Info("Have post file body")
+
+	return
+}
+
+func HandleStop(posts map[string]Post) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		logrus.Info("Received request to stop all listener channels")
+
 		status := http.StatusOK
 		for _, post := range posts {
 			if err := srv.Channels.Stop(post.Channel).Do(); err != nil {
@@ -186,76 +242,9 @@ func startHTTPListener(posts map[string]Post) {
 				status = http.StatusInternalServerError
 			}
 		}
+
 		w.WriteHeader(status)
 		logrus.Info("Exiting...")
 		os.Exit(0)
-	})
-	if err := http.ListenAndServe(":9000", router); err != nil {
-		logrus.WithError(err).Fatal("error starting http listener")
 	}
-}
-
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		logrus.WithError(err).Fatal("Unable to read authorization code")
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		logrus.WithError(err).Fatal("Unable to retrieve token from file")
-	}
-	return tok
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	logrus.WithField("path", path).Info("Saving credential file")
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		logrus.WithError(err).Fatal("Unable to cache oauth tok")
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
-
-func generateHash(length int) string {
-	pool := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-	rand.Seed(time.Now().UTC().UnixNano())
-	b := make([]rune, length)
-	for i := range b {
-		b[i] = pool[rand.Intn(len(pool))]
-	}
-	return string(b)
 }
---
+++ utils.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"golang.org/x/oauth2"
+	"google.golang.org/api/drive/v3"
+)
+
+var srv *drive.Service
+
+func generateHash(length int) string {
+	pool := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	rand.Seed(time.Now().UTC().UnixNano())
+	b := make([]rune, length)
+	for i := range b {
+		b[i] = pool[rand.Intn(len(pool))]
+	}
+	return string(b)
+}
+
+/*****************************
+* google drive service setup *
+*****************************/
+
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	// The file token.json stores the user's access and refresh tokens, and is
+	// created automatically when the authorization flow completes for the first
+	// time.
+	tokFile := "token.json"
+	tok, err := tokenFromFile(tokFile)
+	if err != nil {
+		tok = getTokenFromWeb(config)
+		saveToken(tokFile, tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		logrus.WithError(err).Fatal("Unable to read authorization code")
+	}
+
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		logrus.WithError(err).Fatal("Unable to retrieve token from file")
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(file string) (*oauth2.Token, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	return tok, err
+}
+
+// Saves a token to a file path.
+func saveToken(path string, token *oauth2.Token) {
+	logrus.WithField("path", path).Info("Saving credential file")
+	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		logrus.WithError(err).Fatal("Unable to cache oauth tok")
+	}
+	defer f.Close()
+	json.NewEncoder(f).Encode(token)
+}
