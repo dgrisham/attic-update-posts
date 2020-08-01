c8273f6da6888f8734785597ed13fd1eae081a4f
dgrisham <dmgrisham@gmail.com>
2020-08-01 17:53:58 -0600

    temp: always recreate thumbnails

--- main.go
+++ main.go
@@ -383,7 +383,7 @@ func downloadPost(post Post) error {
 	* if we downloaded a new cover image, generate thumbnails *
 	**********************************************************/
 
-	if imageDownloaded {
+	{
 		var args []string
 		title := strings.TrimSuffix(post.FileName, filepath.Ext(post.FileName))
 		args = append(args, "/home/grish/html/bin/make_thumbnail.zsh", title, post.Author, imagePath, htmlDirectory)
